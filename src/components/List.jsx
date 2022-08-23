c0da72336f977a4011cd11d60d8f308a5b6b0fb4
sangboking <cowg001@naver.com>
2022-08-23 17:15:14 +0900

    feat : react-beautiful-dnd todo drag 기능 추가

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 export default function List({ todoData, setTodoData }) {
 
@@ -18,40 +19,66 @@ export default function List({ todoData, setTodoData }) {
     setTodoData(newTodoData);
   };
 
+  const handleEnd = (result) => {
+    if(!result.destination) return;
+
+    const newTodoData = [...todoData];
+    const [reOrderArr] = newTodoData.splice(result.source.index, 1);
+    newTodoData.splice(result.destination.index, 0, reOrderArr);
+    setTodoData(newTodoData);
+  };
+
   return (
     <div>
-      {
-        todoData.map((data) => (
-          <div
-            className='flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded' 
-            key={data.id}
-          >
-            <div
-              className='item-center'
-            >
-              <input 
-                type='checkbox' 
-                defaultCheckd={false} 
-                onChange={() => handleCompleteChange(data.id)}
-              />
-              <span
-                className={data.completed ? "line-through" : undefined}
-              >
-               {data.title}
-              </span>
+      <DragDropContext onDragEnd={handleEnd}>
+        <Droppable droppableId='todo'>
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+            {
+              todoData.map((data, index) => (
+                <Draggable key={data.id} draggableId={data.id.toString()} index={index}>
+                  {(provided, snapshot) => (
+                    <div 
+                      className={`${
+                      snapshot.isDragging ? "bg-gray-400" : "bg-gray-100" 
+                      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`} 
+                      key={data.id}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                    >
+                      <div className='item-center'>
+                        <input 
+                          type='checkbox' 
+                          defaultCheckd={data.completed} 
+                          onChange={() => handleCompleteChange(data.id)}
+                        />
+                        <span 
+                          className={
+                            data.completed ? "line-through" : undefined
+                          }
+                        >
+                        {data.title}
+                        </span>
+                      </div>
+                    
+                      <div>
+                        <button 
+                          className='px-4 py-2 float-right' 
+                          onClick={() => handleClick(data.id)}
+                        >
+                          x
+                        </button>        
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
             </div>
-           
-            <div>
-              <button
-                className='px-4 py-2 float-right' 
-                onClick={() => handleClick(data.id)}
-              >
-                x
-              </button>        
-            </div>
-          </div>
-        ))
-        }
+          )}   
+        </Droppable>
+      </DragDropContext>   
     </div>
   )
 };
