12b4afdf71a9e41cdb670c6028df9e143d52e52d
sangboking <cowg001@naver.com>
2022-08-29 14:22:57 +0900

    feat : todo 전체삭제기능, todoData localstorage 저장 기능 추가

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Title = React.memo(() => {
+const Title = React.memo(({ handleRemoveClick }) => {
   return (
     <div className="flex justify-between mb-3">
       <h1 className="text-gray-1000 text-lg">할 일 목록</h1>
+      <button type="submit" onClick={handleRemoveClick}>
+        모두 삭제
+      </button>
     </div>
   );
 });
